53550d14e2fe72b134bf8cf10268da0bb22e597b
magnesae <opaduel@hotmail.com>
2022-03-18 15:39:27 +0900

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 // App.tsx
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, useDisclosure } from '@chakra-ui/react';
 import Layout from './components/Layout';
+import ConnectButton from './components/ConnectButton';
+import AccountModal from './components/AccountModal';
+// import '@fontsource/inter';
 
 export default function App() {
+  // Pull the disclosure methods
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     // lets us use Chakra UI syntax across our app:
     <ChakraProvider>
       <Layout>
-        <p style={{ color: 'white' }}>Hello, world!</p>
-      </Layout>{' '}
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Layout>
     </ChakraProvider>
   );
 }
